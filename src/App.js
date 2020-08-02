fb2492aa17b7a332f7b55c04fac53e4d013da28a
SudoTech Code <sudotechcode@gmail.com>
2020-08-03 01:13:24 +0700

    finishing project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+var zxcvbn = require("zxcvbn")
 function App() {
+  const [type, setType] = useState("input");
+  const [score, setScore] = useState("null");
+
+  const showHide = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    let currentType = type === "input" ? "password" : "input"
+    setType(currentType)
+  }
+
+  const testStrengthPassword = (e) => {
+    // we will get score property from zxcvbn
+    if (e.target.value !== "") {
+      let pass = zxcvbn(e.target.value)
+      setScore(pass.score)
+    }else{
+      setScore("null")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <label className="label-password">
+      Password
+      <input 
+        type={type}
+        className="input-password"
+        onChange={testStrengthPassword}
+      />
+      <span className="show-password" onClick={showHide}>
+        {type === "input" ? "Hide" : "Show"}
+      </span>
+      <span
+        className="strength-password"
+        data-score={score}
+      />
+    </label>
   );
 }
 
